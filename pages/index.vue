28c86f5bea8892e7dd9c4ad89a11e359ef3956a3
Roman <roman.bronevik@ya.ru>
2019-02-28 17:27:42 +0300

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -70,7 +70,13 @@
         </div>
 
         <div class="row text-left align-items-center pt-5 pb-md-5">
-          <div class="col-lg-5 col-md-5 col-sm-12 m-md-auto order-md-5" style="z-index: 10000;"><p><img alt="image" class="img-fluid fr-fic fr-dii" src="/images/profile_photos.png"></p></div>
+          <div class="col-lg-5 col-md-5 col-sm-12 m-md-auto order-md-5" style="z-index: 10000;"><p><img alt="image" class="img-fluid fr-fic fr-dii" src="/images/recipe_collections.png"></p></div>
+
+          <div class="col-12 col-md-5 m-md-auto" style="z-index: 10000;"><h2><strong>Добавляй рецепты в коллекции</strong></h2><p class="lead">Блок с коллекциями пользователя - можно делать свои подборки рецептов: подборка блюд на новый год, рецепты низкокаллорийных тортов :)</p></div>
+        </div>
+
+        <div class="row text-left align-items-center pt-5 pb-md-5">
+          <div class="col-lg-5 col-md-5 col-sm-12" style="z-index: 10000;"><p><img alt="image" class="img-fluid fr-fic fr-dii" src="/images/profile_photos.png"></p></div>
 
           <div class="col-12 col-md-5 m-md-auto" style="z-index: 10000;"><h2><strong>Не только рецепты</strong></h2><p class="lead">Блок с фотографиями пользователя - в какое кафе любит ходить с семьей, где лучше мороженое, фото праздничного стола по случаю дня рождения и др :)</p></div>
         </div>
